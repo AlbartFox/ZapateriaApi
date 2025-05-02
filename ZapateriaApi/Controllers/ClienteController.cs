db446545a865de1743247b365ccabd0df329f3a0
AlbartFox <albart24200012@gmail.com>
2025-05-01 22:11:50 -0600

    Listar Clientes

---
+++ ZapateriaApi/Controllers/ClienteController.cs
@@ -0,0 +1,155 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using Oracle.EntityFrameworkCore;
+using System.Data;
+using Microsoft.AspNetCore.Mvc;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+namespace ZapateriaApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public ClienteController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet("listar")]
+        public async Task<IActionResult> GetVistaPlanilla()
+        {
+            string connectionString = _configuration.GetConnectionString("connectionDB");
+
+            try
+            {
+                using (OracleConnection connection = new OracleConnection(connectionString))
+                {
+                    await connection.OpenAsync();
+
+                    string sql = "SELECT * FROM cliente";
+
+                    using (OracleCommand command = new OracleCommand(sql, connection))
+                    {
+                        using (OracleDataReader reader = await command.ExecuteReaderAsync())
+                        {
+                            var resultados = new List<object>();
+
+                            while (await reader.ReadAsync())
+                            {
+                                var item = new
+                                {
+                                    ID_CLIENTE = reader["ID_CLIENTE"],
+                                    NOMBRE = reader["NOMBRE"],
+                                    APELLIDO = reader["APELLIDO"],
+                                    DIRECCION = reader["DIRECCION"],
+                                    COD_EMPRESA = reader["COD_EMPRESA"],
+                                    ID_USUARIO = reader["ID_USUARIO"],
+                                    ESTADO = reader["ESTADO"]
+                                };
+
+                                resultados.Add(item);
+                            }
+
+                            return Ok(resultados);
+                        }
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Error al acceder a Oracle: {ex.Message}");
+            }
+        }
+
+        /*
+
+        [HttpGet("vista-proveedores")]
+        public async Task<IActionResult> GetProveedor()
+        {
+            string connectionString = _configuration.GetConnectionString("connectionDB");
+
+            try
+            {
+                using (OracleConnection connection = new OracleConnection(connectionString))
+                {
+                    await connection.OpenAsync();
+
+                    string sql = "SELECT * FROM proveedor";
+
+                    using (OracleCommand command = new OracleCommand(sql, connection))
+                    {
+                        using (OracleDataReader reader = await command.ExecuteReaderAsync())
+                        {
+                            var resultados = new List<object>();
+
+                            while (await reader.ReadAsync())
+                            {
+                                var item = new
+                                {
+                                    nombre_proveedor = reader["nombre_proveedor"],
+                                    nit = reader["nit"],
+                                    telefono = reader["telefono"],
+                                };
+
+                                resultados.Add(item);
+                            }
+
+                            return Ok(resultados);
+                        }
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Error al acceder a Oracle: {ex.Message}");
+            }
+        }
+        [HttpGet("vista-productos")]
+        public async Task<IActionResult> GetProducto()
+        {
+            string connectionString = _configuration.GetConnectionString("connectionDB");
+
+            try
+            {
+                using (OracleConnection connection = new OracleConnection(connectionString))
+                {
+                    await connection.OpenAsync();
+
+                    string sql = "SELECT * FROM producto";
+
+                    using (OracleCommand command = new OracleCommand(sql, connection))
+                    {
+                        using (OracleDataReader reader = await command.ExecuteReaderAsync())
+                        {
+                            var resultados = new List<object>();
+
+                            while (await reader.ReadAsync())
+                            {
+                                var item = new
+                                {
+                                    nombre_producto = reader["nombre_producto"],
+                                    descripcion_producto = reader["descripcion_producto"],
+                                    precio_compra = reader["precio_compra"],
+                                    precio_venta = reader["precio_venta"],
+                                    cantidad = reader["cantidad"],
+                                    url = reader["url"],
+                                };
+
+                                resultados.Add(item);
+                            }
+
+                            return Ok(resultados);
+                        }
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Error al acceder a Oracle: {ex.Message}");
+            }
+        }*/
+    }
+}
